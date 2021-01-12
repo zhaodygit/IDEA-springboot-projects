4f477a598ffda6c695f467293b517c793bb430d1
zhaodaiyun <ssss10000@163.com>
2021-01-12 10:58:06 +0800

    aaa

---
+++ 020-springboot-dubbo-interface/src/main/java/com/bjpowernode/springboot/services/StudentService.java
@@ -0,0 +1,9 @@
+package com.bjpowernode.springboot.services;
+
+public interface StudentService {
+    /**
+     * 获取学生人数
+     * @return
+     */
+    Integer queryAllSutdentCount();
+}
---
+++ 020-springboot-dubbo-interface/src/main/java/com/bjpowernode/springboot/services/impl/StudentServiceImpl.java
@@ -0,0 +1,9 @@
+package com.bjpowernode.springboot.services.impl;
+
+import com.bjpowernode.springboot.services.StudentService;
+
+public class StudentServiceImpl implements StudentService {
+    public Integer queryAllSutdentCount() {
+        return null;
+    }
+}
